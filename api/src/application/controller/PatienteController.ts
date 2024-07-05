92900cbc137f5196eaeaac266e43aa9d913934f6
fgurgell50 <137560055+fgurgell50@users.noreply.github.com>
2024-07-05 20:45:58 -0300

    commit 0507

--- api/src/application/controller/PatienteController.ts
+++ api/src/application/controller/PatienteController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express"
 
-
 export default interface PatientController {
     createPatient(req: Request, res: Response): Promise<void>
     createAppointment( req: Request, res: Response): Promise<void>
